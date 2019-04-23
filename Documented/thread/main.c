abe9b337789b9942e3a4d5627d8547531dd2cc02
potados99 <potados99@gmail.com>
2019-04-23 18:10:03 +0900

    Done: thread lab.

--- Documented/thread/main.c
+++ Documented/thread/main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>>
+#include <stdbool.h>
 #include <pthread.h>
+#include <unistd.h>
 
 /**
   오늘 쓸 함수들이 있는데요,
@@ -23,33 +24,59 @@
 		if (!(CONDITION)) {	\
 			DO;		\
 		}			\
-	} while(0)
+	} while(0)			
+
 
 /**
   게으른 프로그래머를 위한 매크로입니다.
   */
-#define ARG_TYPE int
-#define ARG_TYPE_PRIMITIVE
+#define arg_t int
+
+#define ARG_FMT "%d"
+#define ARG_PACK(ARG) (&(ARG))
+#define ARG_UNPACK(ARG) (*((arg_t *)ARG))
 
 
 /**
   pthread 새로 만들 때 가져다 쓸 녀석입니다.
   */
-void thread_callback(void *arg);
+void *thread_callback(void *arg);
 
 
 int main(int argc, char **argv) {
-	
+	pthread_t tid;
+	int result;
+	int arg = 36;
 
+	/* 생성 */
+	result = pthread_create(&tid, NULL, thread_callback, ARG_PACK(arg));	
+	assert(result == 0, perror("pthread_create()"); return -1); /* 조건이 zero로 나오면 에러 출력하고 반환! */
 
+	/* 메인 스레드가 할 일 */
+	for (int i = 0; i < 2; ++ i) {
+		printf("from main thread: counting %d.\n", i);
+		sleep(1);
+	}
+	puts("main thread is done. waiting for new thread.");
+
+	/* 조-인 */	
+	result = pthread_join(tid, NULL); /* blocking */
+	assert(result == 0, perror("pthread_join()"); return -1);
+
+	puts("main thread reaching its end!");
 
 	return 0;
 }
 
-void thread_callback(void *arg) {
-#if (ARG_TYPE_PRIMITIVE > 0) 
-	ARG_TYPE arg_data = 
+void *thread_callback(void *arg) {
+	arg_t arg_data = ARG_UNPACK(arg);
+	printf("argument from main thread: " ARG_FMT "\n", arg_data);
+	
+	/* 새 스레드가 할 일 */
+	for (int i = 0; i < 3; ++i) {
+		printf("from new thread: counting %d.\n", i);	
+		sleep(1);
+	}
 
-#endif
-
+	return NULL;
 }
