2e160af426227bb66484399df491a4173b542316
potados99 <potados99@gmail.com>
2019-04-29 02:31:32 +0900

    Testing

--- Documented/clcd-calc/include/pgpio.h
+++ Documented/clcd-calc/include/pgpio.h
@@ -27,7 +27,8 @@ static bool initialized;
   */
 static inline bool check_init() {
 	if (!initialized) {
-		verbose_out(stderr, "wiringPi is not initialized.\n");
+		print_info("wiringPi is not initialized.\n");
+		
 		return false;
 	} else {
 		return true;
@@ -40,7 +41,8 @@ static inline bool check_init() {
 static inline void gpio_setup() {
 	wiringPiSetup();
 	initialized = true;
-	verbose_out(stdout, "wiringPi setup.\n");
+
+	print_info("wiringPi setup.\n");
 }
 
 /**
@@ -54,10 +56,12 @@ static inline void pin_mode(int pin, int mode) {
 	if (mode == PGPIO_INPUT_PULLUP) {
 		pinMode(pin, PGPIO_INPUT);
 		pullUpDnControl(pin, PUD_UP);
-		verbose_out(stdout, "setting pin %d to input_pullup.\n", pin);
+		
+		print_trace("setting pin %d to input_pullup.\n", pin);
 	} else {
 		pinMode(pin, mode);
-		verbose_out(stdout, "setting pin %d to mode %d.\n", pin, mode);
+		
+		print_trace("setting pin %d to mode %d.\n", pin, mode);
 	}
 }
 
@@ -79,7 +83,7 @@ static inline void digital_write(int pin, int value) {
 	ASSERTDO(check_init(), return);
 
 	digitalWrite(pin, value);
-	verbose_out(stdout, "wrote to pin: %d, with value: %d.\n", pin, value);
+	print_trace("wrote to pin: %d, with value: %d.\n", pin, value);
 }
 
 /**
