a80d765566dfc18513381eac7404247c9746ff5e
potados99 <potados99@gmail.com>
2019-04-30 16:34:39 +0900

    Add: comments in main.c

--- Documented/clcd-calc/src/main.c
+++ Documented/clcd-calc/src/main.c
@@ -1,26 +1,41 @@
 #include "routines.h"
 
 #include "keypad.h"
-#include "calc.h"
 #include "clcd.h"
+#include "calc.h"
 #include "machine_specific.h"
 
 #include <stdlib.h>
 
+/**
+  * Keypad, clcd, calc.
+  * Pointers to instances.
+  *
+  * They will be allocated in heap, when setup() is done.
+  */
 struct keypad 	*mykeypad;	
-struct calc 	*mycalc;
 struct clcd 	*myclcd;
+struct calc 	*mycalc;
 
+/**
+  * Keypad callback.
+  */
 void on_key_pressed(int key_index);
 
+/**
+  * Calc callback.
+  */
 void on_calc_append(int content);
 void on_calc_shift(int content);
 void on_calc_clear(int content);
 
+/**
+  * ENTRY.
+  */
 int main(int argc, char *const argv[]) {
 	setup();
 
-	while(1) {
+	while (1) {
 		loop();
 	}
 	
@@ -28,7 +43,9 @@ int main(int argc, char *const argv[]) {
 	return 0;
 }
 
-
+/**
+  * Run once.
+  */
 void setup() {
 	/* temporary things */
 	int rows[] = { KEYPAD_0, KEYPAD_1, KEYPAD_2, KEYPAD_3 }; /* machine_specific */
@@ -56,17 +73,26 @@ void setup() {
 	calc_clear(mycalc, '0');
 }
 
+/**
+  * Run forever.
+  */
 void loop() {
 	keypad_loop(mykeypad);
 }
 
 
+/**
+  * When a key is pressed.
+  */
 void on_key_pressed(int key_index) {
 	enum KEY key = (enum KEY)key_index;
 	
 	calc_key_input(mycalc, key);
 }
 
+/**
+  * When calc has to add a character to display.
+  */
 void on_calc_append(int content) {
 	if (content == '\n') {
 		clcd_set_cursor(myclcd, 16);
@@ -75,10 +101,16 @@ void on_calc_append(int content) {
 	}
 }
 
+/**
+  * When calc has to shift cursor of display.
+  */
 void on_calc_shift(int content) {
 	clcd_move_cursor(myclcd, content);
 }
 
+/**
+  * When calc has to clear display.
+  */
 void on_calc_clear(int content) {
 	clcd_clear(myclcd);
 	if (content) {
