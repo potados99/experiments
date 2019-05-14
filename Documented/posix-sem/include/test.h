7f80b65b89518f24478bbcf10d7304072626caf7
potados99 <potados99@gmail.com>
2019-05-15 00:12:11 +0900

    Add: link rt

---
+++ Documented/posix-sem/include/test.h
@@ -0,0 +1,6 @@
+#ifndef _test_h
+#define _test_h
+
+void test();
+
+#endif /* _test_h */
---
+++ Documented/posix-sem/src/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "test.h"
+
+int main(int argc, const char * argv[]) {
+	test();
+
+	return 0;
+}
