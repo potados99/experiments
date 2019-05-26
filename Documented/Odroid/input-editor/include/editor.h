9586d7d7ebba37d8c740161fbd90f6f0adfaaec1
potados99 <potados99@gmail.com>
2019-05-27 03:30:20 +0900

    Implementing

--- Documented/Odroid/input-editor/include/editor.h
+++ Documented/Odroid/input-editor/include/editor.h
@@ -6,12 +6,141 @@
  *
  */
 
+#include "macros.h"
+#include "verbose.h"
+
 #include <stdbool.h>
+
+typedef void (*editor_callback)(int);
+
+struct editor_callbacks{
+    editor_callback on_cursor_move;
+    editor_callback on_insert;
+    editor_callback on_delete;
+    editor_callback on_replace;
+};
 
 struct editor {
     bool initialized;
     
+    bool typing;
+    char last_typed;
     
+    struct editor_callbacks callbacks;
 };
+
+/**
+ * Map key index to editor key.
+ */
+enum editor_key {
+    KEY_DQZ = 0,
+    KEY_ABC = 1,
+    KEY_DEF = 2,
+    KEY_GHI = 4,
+    KEY_JKL = 5,
+    KEY_MNO = 6,
+    KEY_PRS = 8,
+    KEY_TUV = 9,
+    KEY_WXY = 10,
+    
+    KEY_CL = 7,     /* cursor left */
+    KEY_CR = 15,    /* cursor right */
+    KEY_DEL = 11,
+    KEY_SPC = 13    /* space */
+};
+
+/**
+ * Get next character sequence of same button from current character.
+ * The sequence is hardcoded.
+ */
+static inline int next_char(char c) {
+    /**
+     * .qz
+     * abc
+     * def
+     *
+     * ghi
+     * jkl
+     * mno
+     *
+     * prs
+     * tuv
+     * wxy
+     */
+    
+    switch (c) {
+        case '.': return 'q';
+        case 'q': return 'z';
+        case 'z': return '.';
+            
+        case 'a': return 'b';
+        case 'b': return 'c';
+        case 'c': return 'a';
+            
+        case 'd': return 'e';
+        case 'e': return 'f';
+        case 'f': return 'd';
+            
+        case 'g': return 'h';
+        case 'h': return 'i';
+        case 'i': return 'g';
+            
+        case 'j': return 'k';
+        case 'k': return 'l';
+        case 'l': return 'j';
+            
+        case 'm': return 'n';
+        case 'n': return 'o';
+        case 'o': return 'm';
+            
+        case 'p': return 'r';
+        case 'r': return 's';
+        case 's': return 'p';
+            
+        case 't': return 'u';
+        case 'u': return 'v';
+        case 'v': return 't';
+            
+        case 'w': return 'x';
+        case 'x': return 'y';
+        case 'y': return 'w';
+            
+        default: return -1;
+    }
+}
+
+/**
+ * Launch callback if not null.
+ */
+static inline int launch_callback(editor_callback callback, int param) {
+    ASSERTDO((callback != NULL), print_error("launch_callback: callback is null.\n"); return -1);
+
+    callback(param);
+    
+    return 0;
+}
+
+/**
+ * Callback launchers
+ */
+int _editor_on_cursor_move(struct editor *editor, int delta);
+int _editor_on_insert(struct editor *editor, char c);
+int _editor_on_delete(struct editor *editor);
+int _editor_on_replace(struct editor *editor, char c);
+
+/**
+ * Initial setup.
+ */
+int editor_setup(struct editor *editor);
+
+/**
+ * Add callbacks.
+ */
+int editor_add_callbacks(struct editor *editor, struct editor_callbacks callbacks);
+
+/**
+ * Do action given the index of the key pressed.
+ */
+int editor_input(int key_index);
 
 #endif /* _EDITOR_H */
--- Documented/Odroid/input-editor/src/editor.c
+++ Documented/Odroid/input-editor/src/editor.c
@@ -1,3 +1,149 @@
 #include "editor.h"
+#include "verbose.h"
+#include "macros.h"
+
+/*
+ struct editor {
+ bool initialized;
+ 
+ bool typing;
+ char last_typed;
+ 
+ struct editor_callbacks callbacks;
+ };
+ */
 
 
+int _editor_on_cursor_move(struct editor *editor, int delta) {
+    return launch_callback(editor->callbacks.on_cursor_move , delta);
+}
+
+int _editor_on_insert(struct editor *editor, char c) {
+    return launch_callback(editor->callbacks.on_insert , c);
+}
+
+int _editor_on_delete(struct editor *editor) {
+    return launch_callback(editor->callbacks.on_delete , 0);
+}
+
+int _editor_on_replace(struct editor *editor, char c) {
+    return launch_callback(editor->callbacks.on_replace , c);
+}
+
+int editor_setup(struct editor *editor) {
+    ASSERTDO((editor != NULL), print_error("editor_setup: editor is null.\n"); return -1);
+
+    editor->initialized = false;
+    
+    editor->typing = false;
+    editor->last_typed = 0;
+    
+    editor->initialized = true;
+
+    return 0;
+}
+
+int editor_add_callbacks(struct editor *editor, struct editor_callbacks callbacks) {
+    ASSERTDO((editor != NULL), print_error("editor_add_callbacks: editor is null.\n"); return -1);
+    
+    ASSERTDO((callbacks.on_cursor_move != NULL),
+             print_error("editor_add_callbacks: callbacks.on_cursor_move is null.\n");
+             return -1);
+    
+    ASSERTDO((callbacks.on_insert != NULL),
+             print_error("editor_add_callbacks: callbacks.on_insert is null.\n");
+             return -1);
+    
+    ASSERTDO((callbacks.on_delete != NULL),
+             print_error("editor_add_callbacks: callbacks.on_delete is null.\n");
+             return -1);
+    
+    ASSERTDO((callbacks.on_replace != NULL),
+             print_error("editor_add_callbacks: callbacks.on_replace is null.\n");
+             return -1);
+    
+    editor->callbacks = callbacks;
+    
+    return 0;
+}
+
+int editor_input(int key_index) {
+    
+    /*
+     KEY_DQZ
+     KEY_ABC
+     KEY_DEF
+     KEY_GHI
+     KEY_JKL
+     KEY_MNO
+     KEY_PRS
+     KEY_TUV
+     KEY_WXY
+     KEY_CL
+     KEY_CR
+     KEY_DEL
+     KEY_SPC
+
+     */
+    
+    switch (key_index) {
+        case KEY_DQZ:
+            
+            break;
+            
+        case KEY_ABC:
+        
+            break;
+            
+        case KEY_DEF:
+            
+            break;
+            
+        case KEY_GHI:
+            
+            break;
+            
+        case KEY_JKL:
+            
+            break;
+            
+        case KEY_MNO:
+            
+            break;
+            
+        case KEY_PRS:
+            
+            break;
+            
+        case KEY_TUV:
+            
+            break;
+            
+        case KEY_WXY:
+            
+            break;
+            
+        case KEY_CL:
+            
+            break;
+            
+        case KEY_CR:
+            
+            break;
+            
+        case KEY_DEL:
+            
+            break;
+            
+        case KEY_SPC:
+            
+            break;
+            
+        default:
+            break;
+    }
+    
+    
+    return 0;
+    
+}
