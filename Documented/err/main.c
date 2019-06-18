8b7030820c0cc36c9327aaf8b03fd05936b19a1c
potados99 <potados99@gmail.com>
2019-06-18 23:24:40 +0900

    Add some comments

--- Documented/err/main.c
+++ Documented/err/main.c
@@ -4,6 +4,7 @@
 #include <errno.h>
 #include <err.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 void errorme() {
 	fprintf(stderr, "my-gorgeous-app:");
@@ -27,17 +28,21 @@ int main(int argc, char * argv[]) {
 		printf("%d:\t<+%d>\tat %p\t\"%s\"\n", i, cur_str - origin, cur_str, cur_str);
 	}
 
-	char *ag = "ky";
-
 	// fopen(ag, "r");
 
 //	perror("perror called");
 
 //	error_print_progname = errorme;
 
-	error_at_line(errno, errno, __FILE__, __LINE__, "\"%s\"", ag);
 
-	bool a = true;
+	puts("");
+	
+	malloc(-1);
+	//error_at_line(0, errno, __FILE__, __LINE__, "%s", "error_at_line() called");
+	error(1, 0, "error called"); /* first parameter is a number that will be returned to console, when the second one is non-zero. */
+	err(1, "err called."); /* similar to error, but the second parameter of that is not given. it takes errno as a condition. */
+
+	printf("alive zone\n");
 
 	return 0;
 }
