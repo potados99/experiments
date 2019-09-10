8d7403ba6c163f269573afdecb6c19446455256f
potados99 <potados99@gmail.com>
2019-09-10 11:02:52 +0900

    Add new experiment

---
+++ Documented/arithmetic/include/arithmetic.h
@@ -0,0 +1,13 @@
+#ifndef _arithmetic
+#define _arithmetic
+
+#include <stdbool.h>
+
+bool tadd_ok(int x, int y);
+
+bool tsub_ok(int x, int y);
+
+bool tmult_ok(int x, int y);
+
+
+#endif /* _arithmetic */
---
+++ Documented/arithmetic/src/arithmetic.c
@@ -0,0 +1,16 @@
+#include "arithmetic.h"
+
+bool tadd_ok(int x, int y) {
+	// TODO
+	return true;
+}	
+
+bool tmult_ok(int x, int y) {
+	// TODO
+	return true;
+}
+
+bool tsub_ok(int x, int y) {
+	// TODO
+	return true;
+}
