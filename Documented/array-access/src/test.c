3695446f624b237dc8cdcdb7460908f82dedb5ae
potados99 <potados99@gmail.com>
2019-10-28 23:51:50 +0900

    Add expression for getting linear address for 3d array.

--- Documented/array-access/src/test.c
+++ Documented/array-access/src/test.c
@@ -7,6 +7,19 @@
 TEST(array_access_test) { 
 	int arr[5] = { 32, 12, 35, 53, 11 };
 	ASSERT(0[arr] == 32);
+	ASSERT(1[arr] == 12);
+
+	return true;
+}
+
+TEST(nested_array_test) {
+#define R 24
+#define S 35
+#define T 12
+#define ADDR3D(X, Y, Z) (X*S*T + Y*T + Z)
+
+	long A[R][S][T];
+	ASSERT(&A[1][2][3] == ((long *)A + ADDR3D(1, 2, 3)));
 	
 	return true;
 }
@@ -14,8 +27,9 @@ TEST(array_access_test) {
 /**
  * Apply changes here after modifying tests.
  */
-size_t testc = 1;
+size_t testc = 2;
 
 test_function testv[] = {
-	array_access_test
+	array_access_test,
+	nested_array_test
 };
