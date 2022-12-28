8fd4a54d9721b5e3e84d543799258551ae95a0de
potados99 <potados99@gmail.com>
2022-12-28 21:39:38 +0900

    feat: 누워서 C언어

---
+++ Documented/absinthe/src/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int 	get_age();
+
+int main(int argc, const char * argv[]) {
+	printf("생쟝 사랑해요!\n");
+        printf("생쟝의 나이는 %d쨜!\n", get_age());
+
+        puts("하읏,,하으으,,,,,,아앙!!😍");
+	puts("음쟝 바보!!");
+	return 0;
+}
+
+int get_age() {
+        return 22;
+}
